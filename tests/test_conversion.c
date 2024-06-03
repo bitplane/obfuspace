dceb43908c42a1cf77627b75080228d10e248e65
Gaz Davidson <gaz@bitplane.net>
2024-06-03 21:04:39 +0100

    use assert in tests

--- tests/test_conversion.c
+++ tests/test_conversion.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
+#include <assert.h>
 #include "../src/obfuspace.h"
-
 
 void print_bits(char c) {
     for (int i = 7; i >= 0; i--) {
@@ -8,75 +8,77 @@ void print_bits(char c) {
     }
 }
 
-int test_whitespace_to_crumb() {
-    return WHITESPACE_TO_CRUMB(' ') == 0 &&
-           WHITESPACE_TO_CRUMB('\t') == 1 &&
-           WHITESPACE_TO_CRUMB('\n') == 2 &&
-           WHITESPACE_TO_CRUMB('\r') == 3;
+void test_whitespace_to_crumb() {
+    assert(WHITESPACE_TO_CRUMB(' ') == 0);
+    assert(WHITESPACE_TO_CRUMB('\t') == 1);
+    assert(WHITESPACE_TO_CRUMB('\n') == 2);
+    assert(WHITESPACE_TO_CRUMB('\r') == 3);
+    printf("test_whitespace_to_crumb passed\n");
 }
 
-int test_crumb_to_whitespace() {
-    return CRUMB_TO_WHITESPACE(0) == ' ' &&
-           CRUMB_TO_WHITESPACE(1) == '\t' &&
-           CRUMB_TO_WHITESPACE(2) == '\n' &&
-           CRUMB_TO_WHITESPACE(3) == '\r';
+void test_crumb_to_whitespace() {
+    assert(CRUMB_TO_WHITESPACE(0) == ' ');
+    assert(CRUMB_TO_WHITESPACE(1) == '\t');
+    assert(CRUMB_TO_WHITESPACE(2) == '\n');
+    assert(CRUMB_TO_WHITESPACE(3) == '\r');
+    printf("test_crumb_to_whitespace passed\n");
 }
 
-int test_get_crumb() {
-    return GET_CRUMB(0b11000000, 0) == 3 &&
-           GET_CRUMB(0b00110000, 1) == 3 &&
-           GET_CRUMB(0b00001100, 2) == 3 &&
-           GET_CRUMB(0b00000011, 3) == 3;
+void test_get_crumb() {
+    assert(GET_CRUMB(0b11000000, 0) == 3);
+    assert(GET_CRUMB(0b00110000, 1) == 3);
+    assert(GET_CRUMB(0b00001100, 2) == 3);
+    assert(GET_CRUMB(0b00000011, 3) == 3);
+    printf("test_get_crumb passed\n");
 }
 
-int test_set_crumb() {
-    return SET_CRUMB(3, 0) == 0b11000000 &&
-           SET_CRUMB(3, 1) == 0b00110000 &&
-           SET_CRUMB(3, 2) == 0b00001100 &&
-           SET_CRUMB(3, 3) == 0b00000011;
+void test_set_crumb() {
+    assert(SET_CRUMB(3, 0) == 0b11000000);
+    assert(SET_CRUMB(3, 1) == 0b00110000);
+    assert(SET_CRUMB(3, 2) == 0b00001100);
+    assert(SET_CRUMB(3, 3) == 0b00000011);
+    printf("test_set_crumb passed\n");
 }
 
-int test_get_byte() {
-    // get a byte from a 4-byte integer
+void test_get_byte() {
     unsigned int val = 0x12345678;
-
-    return GET_BYTE(val, 0) == 0x12 &&
-           GET_BYTE(val, 1) == 0x34 &&
-           GET_BYTE(val, 2) == 0x56 &&
-           GET_BYTE(val, 3) == 0x78;
+    assert(GET_BYTE(val, 0) == 0x12);
+    assert(GET_BYTE(val, 1) == 0x34);
+    assert(GET_BYTE(val, 2) == 0x56);
+    assert(GET_BYTE(val, 3) == 0x78);
+    printf("test_get_byte passed\n");
 }
 
-int test_set_byte() {
-    // set a byte in a 4-byte integer
+void test_set_byte() {
     unsigned int val = 0x12;
-    return SET_BYTE(val, 0) == 0x12000000 &&
-           SET_BYTE(val, 1) == 0x00120000 &&
-           SET_BYTE(val, 2) == 0x00001200 &&
-           SET_BYTE(val, 3) == 0x00000012;
+    assert(SET_BYTE(val, 0) == 0x12000000);
+    assert(SET_BYTE(val, 1) == 0x00120000);
+    assert(SET_BYTE(val, 2) == 0x00001200);
+    assert(SET_BYTE(val, 3) == 0x00000012);
+    printf("test_set_byte passed\n");
 }
 
-int test_whitespace_to_byte() {
-    //            " \t\n\r"
-    int value = 0x20090a0d;
-    return WHITESPACE_TO_BYTE(value) == 0b00011011;
+void test_whitespace_to_byte() {
+    int value = 0x20090a0d; // " \t\n\r"
+    assert(WHITESPACE_TO_BYTE(value) == 0b00011011);
+    printf("test_whitespace_to_byte passed\n");
 }
 
-int test_byte_to_whitespace() {
-    //            " \t\n\r"
+void test_byte_to_whitespace() {
     int value = 0b00011011;
-    return BYTE_TO_WHITESPACE(value) == 0x20090a0d;
+    assert(BYTE_TO_WHITESPACE(value) == 0x20090a0d); // " \t\n\r"
+    printf("test_byte_to_whitespace passed\n");
 }
-
 
 int main() {
-    printf("test_whitespace_to_crumb: %d\n", test_whitespace_to_crumb());
-    printf("test_crumb_to_whitespace: %d\n", test_crumb_to_whitespace());
-    printf("test_get_crumb: %d\n", test_get_crumb());
-    printf("test_set_crumb: %d\n", test_set_crumb());
-    printf("test_get_byte: %d\n", test_get_byte());
-    printf("test_set_byte: %d\n", test_set_byte());
-    printf("test_whitespace_to_byte: %d\n", test_whitespace_to_byte());
-    printf("test_byte_to_whitespace: %d\n", test_byte_to_whitespace());
+    test_whitespace_to_crumb();
+    test_crumb_to_whitespace();
+    test_get_crumb();
+    test_set_crumb();
+    test_get_byte();
+    test_set_byte();
+    test_whitespace_to_byte();
+    test_byte_to_whitespace();
 
     return 0;
 }
